3b0903574a83d7cb727b724163cdc75f73ee3600
Sujal Jain <sujaljain0710@gmail.com>
2026-06-06 13:35:13 +0530

    Prime Program Amended

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,5 +1,28 @@
-class Main{
-   public static void main(String[] args){
-   
-   }
-   }
+class Main {
+    public static void main(String[] args) {
+    
+    int num=2;
+    boolean isPrime = true;
+    
+    if(num <= 1){
+        isPrime = false;
+    }
+    else{
+        for(int i=2;i < num;i++){
+             if (num % i == 0) {
+             isPrime = false;
+             break;
+        }
+      }
+    }    
+    
+    if(isPrime){
+        System.out.println("Prime Number");
+    }
+    else{
+        System.out.println("Not a Prime Number");
+    }
+    
+        
+    }
+}
